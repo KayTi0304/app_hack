88d8b8d2537ade6b7758339db610c6d452c53551
mti <mti@purdue.edu>
2022-01-22 00:12:01 -0500

    extra stuff 1

--- src/Pages/Marketplace.jsx
+++ src/Pages/Marketplace.jsx
@@ -1,5 +1,29 @@
 import "./Marketplace.css";
 
+const farmers_data = [
+  {
+    email: "farmer1@gmail.com",
+    password: "farmer1",
+    name: "farmer1",
+    description: "we make corns and chicken",
+    products: ["corns", "chicken"],
+  },
+  {
+    email: "farmer2@gmail.com",
+    password: "farmer2",
+    name: "farmer2",
+    description: "we make beans and beef",
+    products: ["beans", "beef"],
+  },
+  {
+    email: "farmer3@gmail.com",
+    password: "farmer3",
+    name: "farmer3",
+    description: "we make wheat and lamb",
+    products: ["wheat", "lamb"],
+  },
+];
+
 const Marketplace = () => {
   return (
     <div>
@@ -10,11 +34,22 @@ const Marketplace = () => {
         <button className="btn-market">Restaurants</button>
       </section>
       <section>
-        <div className="cardprop">
-          <p>Company name</p>
-          <p>Description</p>
-          <p>Products</p>
-        </div>
+        <ul className="ulmarket">
+          {farmers_data.map((farmer) => (
+            <div className="cardprop">
+              <p>{farmer.email}</p>
+              <p>{farmer.name}</p>
+              <p>{farmer.description}</p>
+              <p>
+                <div>
+                  {farmer.products.map((product) => (
+                    <text>{product} </text>
+                  ))}
+                </div>
+              </p>
+            </div>
+          ))}
+        </ul>
       </section>
     </div>
   );
